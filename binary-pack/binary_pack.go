497ae02dc90277799f8494ba6cd07811d5fe9768
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2017-01-31 15:02:29 +0300

    Implemented converting for bool, int, float and string types

--- binary-pack/binary_pack.go
+++ binary-pack/binary_pack.go
@@ -4,16 +4,93 @@ import (
 	"strings"
 	"strconv"
 	"errors"
+	"encoding/binary"
+	"bytes"
+	"fmt"
 )
 
 type BinaryPack struct {}
 
-func (bp *BinaryPack) Pack(format []string, args ...interface{}) ([]byte, error) {
-	return []byte{}, nil
+func (bp *BinaryPack) Pack(format []string, msg []interface{}) ([]byte, error) {
+	if len(format) > len(msg) {
+		return nil, errors.New("Format is longer than values to pack")
+	}
+
+	res := []byte{}
+
+	for i, f := range format {
+		switch f {
+		case "?":
+			res = append(res, boolToBytes(msg[i].(bool))...)
+		case "h", "H":
+			res = append(res, intToBytes(msg[i].(int), 2)...)
+		case "i", "I", "l", "L":
+			res = append(res, intToBytes(msg[i].(int), 4)...)
+		case "q", "Q":
+			res = append(res, intToBytes(msg[i].(int), 8)...)
+		case "f":
+			res = append(res, float32ToBytes(msg[i].(float32), 4)...)
+		case "d":
+			res = append(res, float64ToBytes(msg[i].(float64), 8)...)
+		default:
+			if strings.Contains(f, "s") {
+				n, _ := strconv.Atoi(strings.TrimRight(f, "s"))
+				res = append(res, []byte(fmt.Sprintf("%s%s",
+					msg[i].(string), strings.Repeat("\x00", n - len(msg[i].(string)))))...)
+			} else {
+				return nil, errors.New("Unexpected format token: '" + f + "'")
+			}
+		}
+	}
+
+	return res, nil
 }
 
 func (bp *BinaryPack) UnPack(format []string, msg []byte) ([]interface{}, error) {
-	return make([]interface{}, 1), nil
+	expected_size, err := bp.CalcSize(format)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if expected_size > len(msg) {
+		return nil, errors.New("Expected size is bigger than actual size of message")
+	}
+
+	res := []interface{}{}
+
+	for _, f := range format {
+		switch f {
+		case "?":
+			res = append(res, bytesToBool(msg[:1]))
+			msg = msg[1:]
+		case "h", "H":
+			res = append(res, bytesToInt(msg[:2]))
+			msg = msg[2:]
+		case "i", "I", "l", "L":
+			res = append(res, bytesToInt(msg[:4]))
+			msg = msg[4:]
+		case "q", "Q":
+			res = append(res, bytesToInt(msg[:8]))
+			msg = msg[8:]
+		case "f":
+			res = append(res, bytesToFloat32(msg[:4]))
+			msg = msg[4:]
+		case "d":
+			res = append(res, bytesToFloat64(msg[:8]))
+			msg = msg[8:]
+		default:
+			if strings.Contains(f, "s") {
+				n, _ := strconv.Atoi(strings.TrimRight(f, "s"))
+				res = append(res, string(msg[:n]))
+				msg = msg[n:]
+			} else {
+				return nil, errors.New("Unexpected format token: '" + f + "'")
+			}
+		}
+	}
+
+	return res, nil
 }
 
 func (bp *BinaryPack) CalcSize(format []string) (int, error) {
@@ -25,9 +102,9 @@ func (bp *BinaryPack) CalcSize(format []string) (int, error) {
 			size = size + 1
 		case "h", "H":
 			size = size + 2
-		case "i", "I", "f":
+		case "i", "I", "l", "L", "f":
 			size = size + 4
-		case "Q", "d":
+		case "q", "Q", "d":
 			size = size + 8
 		default:
 			if strings.Contains(f, "s") {
@@ -40,4 +117,70 @@ func (bp *BinaryPack) CalcSize(format []string) (int, error) {
 	}
 
 	return size, nil
+}
+
+func boolToBytes(x bool) []byte {
+	if x {
+		return intToBytes(1, 1)
+	}
+	return intToBytes(0, 1)
+}
+
+func bytesToBool(b []byte) bool {
+	return bytesToInt(b) > 0
+}
+
+func intToBytes(n int, size int) []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, int64(n))
+	return buf.Bytes()[0:size]
+}
+
+func bytesToInt(b []byte) int {
+	buf := bytes.NewBuffer(b)
+
+	switch len(b) {
+	case 1:
+		var x int8
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	case 2:
+		var x int16
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	case 4:
+		var x int32
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	default:
+		var x int64
+		binary.Read(buf, binary.LittleEndian, &x)
+		return int(x)
+	}
+}
+
+func float32ToBytes(n float32, size int) []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, n)
+	return buf.Bytes()[0:size]
+}
+
+func bytesToFloat32(b []byte) float32 {
+	var x float32
+	buf := bytes.NewBuffer(b)
+	binary.Read(buf, binary.LittleEndian, &x)
+	return x
+}
+
+func float64ToBytes(n float64, size int) []byte {
+	buf := bytes.NewBuffer([]byte{})
+	binary.Write(buf, binary.LittleEndian, n)
+	return buf.Bytes()[0:size]
+}
+
+func bytesToFloat64(b []byte) float64 {
+	var x float64
+	buf := bytes.NewBuffer(b)
+	binary.Read(buf, binary.LittleEndian, &x)
+	return x
 }
--- binary-pack/binary_pack_test.go
+++ binary-pack/binary_pack_test.go
@@ -1,6 +1,9 @@
 package binary_pack
 
-import "testing"
+import (
+	"testing"
+	"reflect"
+)
 
 func TestBinaryPack_CalcSize(t *testing.T) {
 	cases := []struct {
@@ -12,6 +15,8 @@ func TestBinaryPack_CalcSize(t *testing.T) {
 		{[]string{"I", "I", "I", "4s"}, 16, false},
 		{[]string{"H", "H", "I", "H", "8s", "H"}, 20, false},
 		{[]string{"i", "?", "H", "f", "d", "h", "I", "5s"}, 30, false},
+		{[]string{"?", "h", "H", "i", "I", "l", "L", "q", "Q", "f", "d", "1s"}, 50, false},
+		// Unknown tokens
 		{[]string{"a", "b", "c"}, 0, true},
 	}
 
@@ -24,6 +29,98 @@ func TestBinaryPack_CalcSize(t *testing.T) {
 
 		if err == nil && got != c.want {
 			t.Errorf("CalcSize(%v) == %d want %d", c.in, got, c.want)
+		}
+	}
+}
+
+func TestBinaryPack_Pack(t *testing.T) {
+	cases := []struct {
+		f []string
+		a []interface{}
+		want []byte
+		e bool
+	}{
+		{[]string{"?", "?"}, []interface{}{true, false}, []byte{1, 0}, false},
+		{[]string{"h", "h", "h"}, []interface{}{0, 5, -5},
+			[]byte{0, 0, 5, 0, 251, 255}, false},
+		{[]string{"H", "H", "H"}, []interface{}{0, 5, 2300},
+			[]byte{0, 0, 5, 0, 252, 8}, false},
+		{[]string{"i", "i", "i"}, []interface{}{0, 5, -5},
+			[]byte{0, 0, 0, 0, 5, 0, 0, 0, 251, 255, 255, 255}, false},
+		{[]string{"I", "I", "I"}, []interface{}{0, 5, 2300},
+			[]byte{0, 0, 0, 0, 5, 0, 0, 0, 252, 8, 0, 0}, false},
+		{[]string{"f", "f", "f"}, []interface{}{float32(0.0), float32(5.3), float32(-5.3)},
+			[]byte{0, 0, 0, 0, 154, 153, 169, 64, 154, 153, 169, 192}, false},
+		{[]string{"d", "d", "d"}, []interface{}{0.0, 5.3, -5.3},
+			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 51, 51, 51, 51, 51, 51, 21, 64, 51, 51, 51, 51, 51, 51, 21, 192}, false},
+		{[]string{"1s", "2s", "10s"}, []interface{}{"a", "bb", "1234567890"},
+			[]byte{97, 98, 98, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48}, false},
+		{[]string{"I", "I", "I", "4s"}, []interface{}{1, 2, 4, "DUMP"},
+			[]byte{1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 68, 85, 77, 80}, false},
+		// Wrong format length
+		{[]string{"I", "I", "I", "4s"}, []interface{}{1, 4, "DUMP"}, nil, true},
+		// Wrong format token
+		{[]string{"I", "a", "I", "4s"}, []interface{}{1, 2, 4, "DUMP"}, nil, true},
+	}
+
+	for _, c := range cases {
+		got, err := new(BinaryPack).Pack(c.f, c.a)
+
+		if err != nil && !c.e {
+			t.Errorf("Pack(%v, %v) raised %v", c.f, c.a, err)
+		}
+
+		if err == nil && !reflect.DeepEqual(got, c.want) {
+			t.Errorf("Pack(%v, %v) == %v want %v", c.f, c.a, got, c.want)
+		}
+	}
+}
+
+func TestBinaryPack_UnPack(t *testing.T) {
+	cases := []struct {
+		f []string
+		a []byte
+		want []interface{}
+		e bool
+	}{
+		{[]string{"?", "?"}, []byte{1, 0}, []interface{}{true, false}, false},
+		{[]string{"h", "h", "h"}, []byte{0, 0, 5, 0, 251, 255},
+			[]interface{}{0, 5, -5}, false},
+		{[]string{"H", "H", "H"}, []byte{0, 0, 5, 0, 252, 8},
+			[]interface{}{0, 5, 2300}, false},
+		{[]string{"i", "i", "i"}, []byte{0, 0, 0, 0, 5, 0, 0, 0, 251, 255, 255, 255},
+			[]interface{}{0, 5, -5}, false},
+		{[]string{"I", "I", "I"}, []byte{0, 0, 0, 0, 5, 0, 0, 0, 252, 8, 0, 0},
+			[]interface{}{0, 5, 2300}, false},
+		{[]string{"f", "f", "f"},
+			[]byte{0, 0, 0, 0, 154, 153, 169, 64, 154, 153, 169, 192},
+			[]interface{}{float32(0.0), float32(5.3), float32(-5.3)}, false},
+		{[]string{"d", "d", "d"},
+			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 51, 51, 51, 51, 51, 51, 21, 64, 51, 51, 51, 51, 51, 51, 21, 192},
+			[]interface{}{0.0, 5.3, -5.3}, false},
+		{[]string{"1s", "2s", "10s"},
+			[]byte{97, 98, 98, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48},
+			[]interface{}{"a", "bb", "1234567890"}, false},
+		{[]string{"I", "I", "I", "4s"},
+			[]byte{1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 68, 85, 77, 80},
+			[]interface{}{1, 2, 4, "DUMP"}, false},
+		// Wrong format length
+		{[]string{"I", "I", "I", "4s", "H"}, []byte{1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 68, 85, 77, 80},
+			nil, true},
+		// Wrong format token
+		{[]string{"I", "a", "I", "4s"}, []byte{1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 68, 85, 77, 80},
+			nil, true},
+	}
+
+	for _, c := range cases {
+		got, err := new(BinaryPack).UnPack(c.f, c.a)
+
+		if err != nil && !c.e {
+			t.Errorf("UnPack(%v, %v) raised %v", c.f, c.a, err)
+		}
+
+		if err == nil && !reflect.DeepEqual(got, c.want) {
+			t.Errorf("UnPack(%v, %v) == %v want %v", c.f, c.a, got, c.want)
 		}
 	}
 }
